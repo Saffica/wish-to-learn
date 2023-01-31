6ca6ed7e224e7553878b87a83424f9eb0f075300
Vavilon <vyacheslav.medvedev@itglobal.com>
2023-01-31 22:52:59 +0300

    frontend_init

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Login from './pages/Login';
+
+function App() {
+  return (
+    <div className="wrapper">
+      <Login></Login>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/Login.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Auth from "../components/auth";
+
+export default function Login() {
+    return (
+        <div>
+            <Auth></Auth>
+        </div>
+    );
+}
