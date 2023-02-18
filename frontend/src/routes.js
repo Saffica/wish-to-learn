12cbe2053ebd31e00b9219d4e5042c5cc381d028
Maksim Zheleznyakov <saffm1994@gmail.com>
2023-02-18 23:34:41 +0300

    add private route Course

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,6 +1,8 @@
 import Courses from "./components/Courses";
 import Login from "./components/Login";
-import { COURSES_ROUTE, LOGIN_ROUTE } from "./utils/consts";
+import Course from "./components/Course";
+
+import { COURSES_ROUTE, COURSE_ROUTE, LOGIN_ROUTE } from "./utils/consts";
 
 export const publicRoutes = [
     {
@@ -13,5 +15,9 @@ export const privateRoutes = [
     {
         path: COURSES_ROUTE,
         Components: Courses
+    },
+    {
+        path: COURSE_ROUTE,
+        Components: Course
     }
 ]
