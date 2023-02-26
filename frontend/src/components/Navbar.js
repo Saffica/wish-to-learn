480ce7a4788e70ccef4513ff78811786e5f37d09
Maksim Zheleznyakov <saffm1994@gmail.com>
2023-02-26 23:27:22 +0300

    navbar and chto-to eshe

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,10 +1,31 @@
-import React from "react";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
 
-const Navbar = () => {
+function Navbar() {
+    const user = true;
     return (
-        <div>
-            Navbar
-        </div>
+        <nav className="navbar">
+            <div className="col-1">
+                <Link to="/">
+                    <img src="/logo.png" alt="Logo" className="logo" />
+                </Link>
+                <Link to="/courses">
+                    Список планов
+                </Link>
+                <Link to="/">
+                    О нас
+                </Link>
+            </div>
+            <div className="col-2">
+                <div className="profile">
+                
+                    <Link to="/profile">
+                        <img src="/profile.png" alt="Profile" className="profile-icon" />
+                    </Link>
+                </div>
+            </div>
+        </nav>
     );
 }
 
