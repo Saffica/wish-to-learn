a8e3ffa9f36b1ba34fd2d1f43a2bbe44e69bdd38
Sushko <maksim.sushko@simpleone.ru>
2023-05-03 01:01:28 +0300

    fix

--- frontend/src/components/layout/header/UserMenu/UserMenu.jsx
+++ frontend/src/components/layout/header/UserMenu/UserMenu.jsx
@@ -1,13 +1,11 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import { UserMenuButton } from './UserMenuButton';
 import { UserMenuPopup } from './UserMunuPopup';
-import myPhoto from '../../../../imgs/myPhoto.svg';
 import style from './UserMenu.module.css';
 
 export const UserMenu = ({ isAuthorized }) => {
     const userInfo = 'Максим Сушко';
-    const userPhoto = myPhoto;
     const userMenu = [
         { title: 'План обучения', link: 'courses', id: 1 },
         { title: 'Перейти в профиль', link: 'profile', id: 2 },
@@ -16,23 +14,13 @@ export const UserMenu = ({ isAuthorized }) => {
 
     const [isPopupOpen, setIsPopupOpen] = useState(false);
 
-    const closePopup = () => setIsPopupOpen(false);
-    const handlePopup = () => setIsPopupOpen(!isPopupOpen);
-
-    useEffect(() => {
-        document.addEventListener('click', closePopup);
-        return () => document.removeEventListener('click', closePopup);
-    }, [isPopupOpen]);
 
     return (
         <div
             className={style.user}
             onClick={element => element.stopPropagation()}>
             {isAuthorized ? (
-                <UserMenuButton
-                    handlePopup={handlePopup}
-                    photo={userPhoto}
-                />
+                <UserMenuButton setIsPopupOpen={setIsPopupOpen} isOpen={isPopupOpen}/>
             ) : (
                 <NavLink
                     className={({ isActive }) => (isActive ? `${style.link} ${style.active}` : style.link)}
--- frontend/src/components/layout/header/UserMenu/UserMenuButton.jsx
+++ frontend/src/components/layout/header/UserMenu/UserMenuButton.jsx
@@ -1,18 +1,27 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import style from './UserMenuButton.module.css';
 
-export const UserMenuButton = ({ handlePopup }) => {
+export const UserMenuButton = ({ setIsPopupOpen, isOpen }) => {
     const [isActive, setIsActive] = useState(false);
+    const closePopup = () => {
+        setIsPopupOpen(false);
+        setIsActive(false);
+    }
+
+    useEffect(() => {
+        document.addEventListener('click', closePopup);
+        return () => document.removeEventListener('click', closePopup);
+    }, [isActive]);
 
     return (
-        <div
+        <button
             className={style.burger}
             onClick={() => {
-                handlePopup();
+                setIsPopupOpen(!isOpen);
                 setIsActive(!isActive);
             }}>
             <span className={isActive ? style.active : ''} />
-        </div>
+        </button>
     );
 };
