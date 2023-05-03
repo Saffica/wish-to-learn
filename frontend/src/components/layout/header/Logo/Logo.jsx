4b7c129528997f47136fd04c05cfceac31c0295f
Sushko <maksim.sushko@simpleone.ru>
2023-05-04 01:01:02 +0300

    fix

--- frontend/src/components/layout/header/Logo/Logo.jsx
+++ frontend/src/components/layout/header/Logo/Logo.jsx
@@ -1,8 +1,13 @@
 import React from "react";
+import { NavLink } from 'react-router-dom';
 import style from './Logo.module.css';
 
 export const Logo = () => {
     return (
-        <div className={style.logo}></div>
+        <NavLink
+            className={style.logo}
+            to='/'
+        >
+        </NavLink>
     )
 }
--- frontend/src/pages/Welcome/WelcomePage.jsx
+++ frontend/src/pages/Welcome/WelcomePage.jsx
@@ -1,9 +1,92 @@
-import React from "react";
+import React from 'react';
+import { ReactComponent as Desktop } from '../../imgs/desktop.svg';
+import style from './WelcomePage.module.css';
 
-export const WelcomePage = () => { 
-    return(
-        <div className="welcome-page">
-            Welcome Page
+export const WelcomePage = () => {
+    return (
+        <div className='welcome-page'>
+            <section className={style.general}>
+                <div className={style.wrapper}>
+                    <div className={style.top}>
+                        <p className={style.description}>ПЛАТФОРМА ДЛЯ самообразования</p>
+                        <p className={style.title}>хочу учу</p>
+                        <p className={style.subtitle}>возьми свое обучение под контроль</p>
+                        <button>К списку планов</button>
+                    </div>
+                    <div className={style.items}>
+                        <div className={style.item}>
+                            {<Desktop />}
+                            <div className={style.title}>
+                                Составляй план
+                                обучения
+                            </div>
+                            <div className={style.line}></div>
+                            <div className={style.description}>
+                                Реалистичный и достижимый план поможет добиться успеха в самообучении.
+                            </div>
+                        </div>
+                        <div className={style.item}>
+                            {<Desktop />}
+                            <div className={style.title}>
+                                Составляй план
+                                обучения
+                            </div>
+                            <div className={style.line}></div>
+                            <div className={style.description}>
+                                Реалистичный и достижимый план поможет добиться успеха в самообучении.
+                            </div>
+                        </div>
+                        <div className={style.item}>
+                            {<Desktop />}
+                            <div className={style.title}>
+                                Составляй план
+                                обучения
+                            </div>
+                            <div className={style.line}></div>
+                            <div className={style.description}>
+                                Реалистичный и достижимый план поможет добиться успеха в самообучении.
+                            </div>
+                        </div>
+                        <div className={style.item}>
+                            {<Desktop />}
+                            <div className={style.title}>
+                                Составляй план
+                                обучения
+                            </div>
+                            <div className={style.line}></div>
+                            <div className={style.description}>
+                                Реалистичный и достижимый план поможет добиться успеха в самообучении.
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+
+
+            <section className='registration'>
+                <div className={style.wrapper}>
+                    <div className='registration__left-colum'>
+                        С помощью нашей платформы пользователи получают возможность создавать курсы, которые агрегируют
+                        материалы из различных источников. Они могут отслеживать свой прогресс, устанавливать цели и
+                        делиться курсами с другими пользователями.
+                    </div>
+                    <div className='registration__right-colum'>
+                        Если Вы заинтересованы в улучшении своих навыков или просто хотите научиться чему-то новому,
+                        зачем ждать? Зарегистрируйтесь сегодня и начните свой путь обучения!
+                        <button>Зарегистрироваться</button>
+                    </div>
+                </div>
+            </section>
+            <section className='feedback'>
+                <div className={style.wrapper}>
+                    <div className='feedback__left-column'></div>
+                    <div className='feedback__right-column'>
+                        Остались вопросы или есть предложения?
+                        <button>Напиши нам</button>
+                    </div>
+                </div>
+            </section>
         </div>
-    )
-}
+    );
+};
