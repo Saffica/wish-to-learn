036447c15c40848664f342b0832010c8fca6b206
Sushko <maksim.sushko@simpleone.ru>
2023-05-22 01:16:59 +0300

    fix

--- frontend/src/features/user/ui/user.tsx
+++ frontend/src/features/user/ui/user.tsx
@@ -1,6 +1,7 @@
 import { NavLink } from 'react-router-dom';
 import { BsPersonCircle } from 'react-icons/bs';
 import { HeaderLink } from '../../../shared';
+import profileIconPath from '../../../shared/imgs/profile.png';
 import classes from './user.module.css';
 
 export const User = () => {
@@ -8,18 +9,14 @@ export const User = () => {
 
     return (
         <div className={classes.user}>
-            <div className={classes.link}>
-                {isAuthorized
-                    ? <HeaderLink link='/' title='Выйти' />
-                    : <HeaderLink link='login' title='Войти' />
-                }
-            </div>
-            <div className={classes.photo}>
-                {isAuthorized
-                    ? <NavLink to='profile'><img src='#' alt="profile" /></NavLink>
-                    : <NavLink to='login'><BsPersonCircle className={classes.personCircle} /></NavLink>
-                }
-            </div>
+            {isAuthorized
+                ? <HeaderLink link='/' title='Выйти' />
+                : <HeaderLink link='login' title='Войти' />
+            }
+            {isAuthorized
+                ? <NavLink to='profile'><img src='#' alt="profile" /></NavLink>
+                : <NavLink to='login'><img src={profileIconPath} alt="profile" /></NavLink>
+            }
         </div >
     );
 };
