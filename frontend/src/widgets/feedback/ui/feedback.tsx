0497fcb73c8c8d6fcb378205598f1edad09c525d
Sushko <maksim.sushko@simpleone.ru>
2023-05-22 00:41:44 +0300

    fix

--- frontend/src/widgets/feedback/ui/feedback.tsx
+++ frontend/src/widgets/feedback/ui/feedback.tsx
@@ -1,10 +1,13 @@
 import { Button } from '../../../shared';
+import { getSvgs } from '../lib/getSvgs';
 import classes from './feedback.module.css';
 
 export const Feedback = (): JSX.Element => {
     return (
         <div className={classes.feedback}>
-            <div className={classes.item}></div>
+            <div className={classes.item}>
+                {getSvgs().map(svg => svg)}
+            </div>
             <div className={classes.item}>
                 <p>Остались вопросы или есть предложения?</p>
                 <Button title='Напиши нам' />
